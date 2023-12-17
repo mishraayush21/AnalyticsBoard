517ee4298a39aac90f26bd83e7040009d918addd
mishraayush21 <ayushmishra7878@gmail.com>
2023-12-17 23:05:09 +0530

    add mysql db integration

--- src/main/java/com/fitmart/analyticsBoard/model/AnalyticsData.java
+++ src/main/java/com/fitmart/analyticsBoard/model/AnalyticsData.java
@@ -1,17 +1,45 @@
 package com.fitmart.analyticsBoard.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "analytics_data")
 @Getter
 @Setter
+@Data
 public class AnalyticsData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id", nullable = false, unique = true)
     private String productId;
+
+    @Column(name = "views")
     private int views;
+
+    @Column(name = "sales")
     private int sales;
+
+    @Column(name = "clicks")
     private int clicks;
+
+    @Column(name = "ctr")
     private double ctr;
+
+    @Column(name = "conversion_rate")
     private double conversionRate;
+
+    // Getters and setters
+
+    public AnalyticsData() {
+        // Default constructor
+    }
 
     public AnalyticsData(String productId, int views, int sales, int clicks) {
         this.productId = productId;
@@ -22,20 +50,4 @@ public class AnalyticsData {
         this.conversionRate = ( clicks != 0 ) ? (double) sales / clicks : 0;
     }
 
-
-    public AnalyticsData() {
-    }
-
-
-    @Override
-    public String toString() {
-        return "AnalyticsData{" +
-                "productId='" + productId + '\'' +
-                ", views=" + views +
-                ", sales=" + sales +
-                ", clicks=" + clicks +
-                ", ctr=" + ctr +
-                ", conversionRate=" + conversionRate +
-                '}';
-    }
 }
--- src/main/java/com/fitmart/analyticsBoard/service/AnalyticsDataService.java
+++ src/main/java/com/fitmart/analyticsBoard/service/AnalyticsDataService.java
@@ -1,74 +1,91 @@
 package com.fitmart.analyticsBoard.service;
 
 import com.fitmart.analyticsBoard.model.AnalyticsData;
+import com.fitmart.analyticsBoard.repository.AnalyticsDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class AnalyticsDataService {
 
-    private final Map<String, AnalyticsData> analyticsDataMap = new HashMap<>();
+    @Autowired
+    private AnalyticsDataRepository analyticsDataRepository;
 
     public void updateAnalyticsData(AnalyticsData analyticsData) {
         String productId = analyticsData.getProductId();
-        if (analyticsDataMap.containsKey(productId)) {
-            AnalyticsData existingData = analyticsDataMap.get(productId);
+        AnalyticsData existingData = analyticsDataRepository.findByProductId(productId);
+
+        if (existingData != null) {
             existingData.setViews(existingData.getViews() + analyticsData.getViews());
             existingData.setSales(existingData.getSales() + analyticsData.getSales());
             existingData.setClicks(existingData.getClicks() + analyticsData.getClicks());
         } else {
-            analyticsDataMap.put(productId, analyticsData);
+            analyticsDataRepository.save(analyticsData);
         }
+
         calculateAndStoreMetrics(analyticsData);
     }
 
     private void calculateAndStoreMetrics(AnalyticsData analyticsData) {
         String productId = analyticsData.getProductId();
-        AnalyticsData data = analyticsDataMap.get(productId);
+        AnalyticsData data = analyticsDataRepository.findByProductId(productId);
         data.setCtr((data.getViews() != 0) ? (double) data.getClicks() / data.getViews() : 0);
         data.setConversionRate((data.getClicks() != 0) ? (double) data.getSales() / data.getClicks() : 0);
+
+        // Save the updated metrics back to the database
+        analyticsDataRepository.save(data);
     }
 
     public AnalyticsData getAnalyticsData(String productId) {
-        return analyticsDataMap.get(productId);
+        return analyticsDataRepository.findByProductId(productId);
     }
 
     public Map<String, AnalyticsData> getAllAnalyticsData() {
-        return new HashMap<>(analyticsDataMap);
+        Map<String, AnalyticsData> analyticsDataMap = new HashMap<>();
+        analyticsDataRepository.findAll().forEach(analyticsData -> analyticsDataMap.put(analyticsData.getProductId(), analyticsData));
+        return analyticsDataMap;
     }
 
+    @Transactional
     public void incrementViews(String productId, int count) {
-        if (analyticsDataMap.containsKey(productId)) {
-            AnalyticsData existingData = analyticsDataMap.get(productId);
+        AnalyticsData existingData = analyticsDataRepository.findByProductId(productId);
+
+        if (existingData != null) {
             existingData.setViews(existingData.getViews() + count);
             calculateAndStoreMetrics(existingData);
         } else {
             AnalyticsData newData = new AnalyticsData(productId, count, 0, 0);
-            analyticsDataMap.put(productId, newData);
+            analyticsDataRepository.save(newData);
         }
     }
 
+    @Transactional
     public void incrementClickCount(String productId, int count) {
-        if (analyticsDataMap.containsKey(productId)) {
-            AnalyticsData existingData = analyticsDataMap.get(productId);
+        AnalyticsData existingData = analyticsDataRepository.findByProductId(productId);
+
+        if (existingData != null) {
             existingData.setClicks(existingData.getClicks() + count);
             calculateAndStoreMetrics(existingData);
         } else {
             AnalyticsData newData = new AnalyticsData(productId, 0, count, 0);
-            analyticsDataMap.put(productId, newData);
+            analyticsDataRepository.save(newData);
         }
     }
 
+    @Transactional
     public void incrementSaleCount(String productId, int count) {
-        if (analyticsDataMap.containsKey(productId)) {
-            AnalyticsData existingData = analyticsDataMap.get(productId);
+        AnalyticsData existingData = analyticsDataRepository.findByProductId(productId);
+
+        if (existingData != null) {
             existingData.setSales(existingData.getSales() + count);
             calculateAndStoreMetrics(existingData);
         } else {
             AnalyticsData newData = new AnalyticsData(productId, 0, 0, count);
-            analyticsDataMap.put(productId, newData);
+            analyticsDataRepository.save(newData);
         }
     }
 }
